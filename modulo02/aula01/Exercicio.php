4c97cb47dfdc4c13b2fe9d39252313d75ee22287
Fabio Costa <fabio.costta@yahoo.com.br>
2022-06-17 11:48:26 -0300

    Atualização

--- modulo02/aula01/Exercicio.php
+++ modulo02/aula01/Exercicio.php
@@ -1,2 +1,29 @@
 <?php
 
+declare(strict_types =1);
+
+class Curso { //classe
+    public $cargaHoraria; //atributos
+    public $gradeCurricular; //atributos
+    public $descricaoCurso;
+    public $nomeCurso;
+}
+
+class Professor { //classe
+    public $nome; //atributos
+    public $especializacao; //atributos
+    public $cpf;
+    public $salario;
+    public $metodoEnsino;
+}
+
+class Disciplina { //classe
+    public $nomeDisciplica; //atributos
+    public $descricaoDisciplina; //atributos
+    public $temasPlanejados;
+    public $exercicios;
+    public $provas;
+}
+
+
+
