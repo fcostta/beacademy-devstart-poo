4e8a656cdc7f7278dafbcee3ac79ea902b5028fa
Fabio Costa <fabio.costta@yahoo.com.br>
2022-06-17 10:12:48 -0300

    atualização

---
+++ modulo02/aula01/Aluno.php
@@ -0,0 +1,13 @@
+<?php
+//quando for criar arquivos que representas classes, a primeira letra deverá ser maiuscula
+
+declare(strict_types =1);
+
+class Aluno { //classe
+    public $nome; //atributos
+    public $cpf; //atributos
+}
+
+//Curso
+//Professor
+//Disciplina
---
+++ modulo02/aula01/intro.php
@@ -0,0 +1,25 @@
+<?php
+
+
+$a1 = new Aluno(); // instanciamento
+
+$a1->nome = 'Alessandro';
+$a1->cpf = '123.123.123.23';
+
+
+
+$aluno = [
+    'nome'=>'Alessandro',
+    'cpf'=>'123.123.123-23'
+
+];
+
+$aluno2 = [
+    'Nome'=>'Bruno',
+    'çpf'=>'234.234.234.34',
+];
+
+$aluno3=[
+    'NOME'=>'Raquel',
+    'CPF'=>'345.345.345-45',
+];
