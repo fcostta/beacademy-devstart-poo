e595e5369d8403e31aa852e5897f510c1abe281d
Fabio Costa <fabio.costta@yahoo.com.br>
2022-06-21 22:49:15 -0300

    atualizacao

--- modulo02/aula04/Gestor.php
+++ modulo02/aula04/Gestor.php
@@ -4,21 +4,6 @@ declare(strict_types=1);
 
 class Gestor
 {
-    private string $nome;
-    private string $email;
-    private string $senha;
-    private string $cpf;
     private float $salario;
     private string $horario;
-    
-    public function getNome(): string
-    {
-        return $this->nome;
-    }
-
-    public function setNome(string $nome): void
-    {
-        $this->nome=$nome;
-    }
-
 }
