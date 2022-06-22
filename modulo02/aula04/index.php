2dbe11e91e3855202fe0b902a8e8bc40e6e753e5
Fabio Costa <fabio.costta@yahoo.com.br>
2022-06-21 23:22:56 -0300

    atualizacao

--- modulo02/aula04/index.php
+++ modulo02/aula04/index.php
@@ -6,9 +6,11 @@ include 'Gestor.php';
 
 $c1=new Cliente();
 $c1->setNome('Chiquim');
+$c1->setDataCadastro('01/01/2019');
 
 $g1= new Gestor();
 $g1->setNome('Zezim');
+$g1->setSalario(6000);
 
 var_dump($c1);
 var_dump($g1);
