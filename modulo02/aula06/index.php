9a55e57e13854522d8d6e6f369a2dcf7e70ed0af
Fabio Costa <fabio.costta@yahoo.com.br>
2022-06-25 18:22:48 -0300

    atualiz

--- modulo02/aula06/index.php
+++ modulo02/aula06/index.php
@@ -2,6 +2,8 @@
 
 ini_set('display_errors', 1);
 
+// Para utilizar o composer, temos que ter definido os namespace
+
 
 use Classes\Config\Usuario as UsuarioConfig; // para não ficar precisando escrever o caminho todo,
                                              // podemos utilizar os comandos "use" e "as" para dar um apelido a classe e não precisar ficar digitando o caminho completo do "namespace" toda vez que for chamar uma classe
