d1dc978e2330f5c4c3b2385e9bab5a57f8bd08c5
Fabio Costa <fabio.costta@yahoo.com.br>
2022-06-25 18:07:06 -0300

    Atualiz

--- modulo02/aula06/index.php
+++ modulo02/aula06/index.php
@@ -2,13 +2,6 @@
 
 ini_set('display_errors', 1);
 
-include 'classes/Usuario.php';
-include 'classes/Cliente.php';
-include 'classes/Gestor.php';
-include 'classes/Categoria.php';
-include 'classes/Produto.php';
-
-include 'classes/Config/Usuario.php';
 
 use Classes\Config\Usuario as UsuarioConfig; // para não ficar precisando escrever o caminho todo,
                                              // podemos utilizar os comandos "use" e "as" para dar um apelido a classe e não precisar ficar digitando o caminho completo do "namespace" toda vez que for chamar uma classe
