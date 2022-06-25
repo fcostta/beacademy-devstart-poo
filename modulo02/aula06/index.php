a8de3a29d82b582e977822b0438812af1798ebf8
Fabio Costa <fabio.costta@yahoo.com.br>
2022-06-25 18:00:58 -0300

    Atualiz

--- modulo02/aula06/index.php
+++ modulo02/aula06/index.php
@@ -10,4 +10,15 @@ include 'classes/Produto.php';
 
 include 'classes/Config/Usuario.php';
 
-echo 'Tcharam, Funcionou!';
+use Classes\Config\Usuario as UsuarioConfig; // para não ficar precisando escrever o caminho todo,
+                                             // podemos utilizar os comandos "use" e "as" para dar um apelido a classe e não precisar ficar digitando o caminho completo do "namespace" toda vez que for chamar uma classe
+
+//echo 'Tcharam, Funcionou!';
+
+$us1 = new Classes\Usuario();
+
+//$us2 = new Classes\Config\Usuario(); // comentado para ser reeescrito com o apelido
+$us2 = new UsuarioConfig();
+
+var_dump($us1);
+var_dump($us2);
