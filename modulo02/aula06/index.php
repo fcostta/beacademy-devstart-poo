7f8c77b4ba6179ea82c004147fcb23c1da254419
Fabio Costa <fabio.costta@yahoo.com.br>
2022-06-25 17:21:08 -0300

    atualiz

--- modulo02/aula06/index.php
+++ modulo02/aula06/index.php
@@ -1,1 +1,12 @@
 <?php
+
+ini_set('display_errors', 1);
+
+include 'classes/Usuario.php';
+include 'classes/Cliente.php';
+include 'classes/Gestor.php';
+include 'classes/Categoria.php';
+include 'classes/Produto.php';
+
+include 'classes/Config/Usuario.php';
+
