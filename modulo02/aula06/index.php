f9d4796f284c7d44e57b0dc919863e6cc45a3fa7
Fabio Costa <fabio.costta@yahoo.com.br>
2022-06-25 19:21:03 -0300

    Atualiz

--- modulo02/aula06/index.php
+++ modulo02/aula06/index.php
@@ -3,7 +3,11 @@
 ini_set('display_errors', 1);
 
 // Para utilizar o composer, temos que ter definido os namespace
+// necessario ter criado o "composer.json"
+// realizar o include vendor/autoload.php
 
+
+include 'vendor/autoload.php';
 
 use Classes\Config\Usuario as UsuarioConfig; // para não ficar precisando escrever o caminho todo,
                                              // podemos utilizar os comandos "use" e "as" para dar um apelido a classe e não precisar ficar digitando o caminho completo do "namespace" toda vez que for chamar uma classe
