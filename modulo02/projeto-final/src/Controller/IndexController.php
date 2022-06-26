0cdaf37a0171c30dcfa045aaa1d1515c39b48913
Fabio Costa <fabio.costta@yahoo.com.br>
2022-06-26 17:27:59 -0300

    At Projeto

--- modulo02/projeto-final/src/Controller/IndexController.php
+++ modulo02/projeto-final/src/Controller/IndexController.php
@@ -9,6 +9,7 @@ class IndexController
     public function indexAction(): void
     {
         //echo dirname(__DIR__);
-        include dirname(__DIR__) . '/View/teste.php'; // dirname() pega a pasta um nivel acima
+        //include dirname(__DIR__) . '/View/teste.php'; // dirname() pega a pasta um nivel acima
+        include dirname(__DIR__) . '/View/index/index.php';
     }
 }
