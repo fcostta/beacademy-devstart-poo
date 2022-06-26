9701ed8c6825c30e222c760a315c275c1626c96a
Fabio Costa <fabio.costta@yahoo.com.br>
2022-06-25 23:14:50 -0300

    Atualiz

--- modulo02/aula06/vendor/composer/autoload_real.php
+++ modulo02/aula06/vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit11abf694e0b3772652af52234380e4c8
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit11abf694e0b3772652af52234380e4c8', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit11abf694e0b3772652af52234380e4c8', 'loadClassLoader'));
