61cedf48aa0123e09d1d165576cce4b5026c5d13
Fabio Costa <fabio.costta@yahoo.com.br>
2022-06-29 22:46:58 -0300

    At Projeto

--- modulo02/projeto-final/public/index.php
+++ modulo02/projeto-final/public/index.php
@@ -11,16 +11,36 @@ use App\Controller\ErrorController;
 
 $url= explode('?',$_SERVER['REQUEST_URI'])[0];
 
-if ($url === '/'){
-    $c= new IndexController();
-    $c->indexAction();
-} elseif ($url === '/login'){
-    $c= new IndexController();
-    $c->loginAction();
-} elseif ($url === '/produtos') {
-    $p = new ProductController();
-    $p->listAction();
-} else {
-    $error= new ErrorController();
-    $error-> notFoundAction();
+$routes =[ // Array associativo
+    '/'=> [
+            'controller' => IndexController::class, // quaqndo coloca ::class, esta invocando o caminho dela "App\Controller\ProductController"
+            'method' => 'indexAction',
+    ],
+    '/produtos'=> [
+            'controller'=> ProductController::class,
+            'method' => 'listAction',
+    ],
+];
+
+if (false === isset($routes[$url])){
+    $e=new ErrorController();
+    $e->notFoundAction();
+    exit;
 }
+
+var_dump($routes[$url]);
+
+
+// if ($url === '/'){
+//     $c= new IndexController();
+//     $c->indexAction();
+// } elseif ($url === '/login'){
+//     $c= new IndexController();
+//     $c->loginAction();
+// } elseif ($url === '/produtos') {
+//     $p = new ProductController();
+//     $p->listAction();
+// } else {
+//     $error= new ErrorController();
+//     $error-> notFoundAction();
+// }
